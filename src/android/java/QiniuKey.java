879e6e1c7bcc069a0eb69c8e2bfd665926f46a44
yumemor <yyzeihaha@gmail.com>
2015-11-12 19:16:21 +0800

    删除token

--- src/android/java/QiniuKey.java
+++ src/android/java/QiniuKey.java
@@ -11,7 +11,7 @@ public interface QiniuKey {
 	 */
 	//Token生成算法：http://developer.qiniu.com/docs/v6/api/overview/security.html#upload-token
 	//Token在线生成地址：http://jsfiddle.net/gh/get/extjs/4.2/icattlecoder/jsfiddle/tree/master/uptoken
-	String UPLOAD_TOKEN = "BB9QNdZo45ZlUI2QbSjh2NQxQFKDMQWEUPitOu9k:UpZPDG8TJ3lZe5R2HrTwbMc8sLg=:eyJzY29wZSI6InFjZGVtbyIsImRlYWRsaW5lIjoxNDQ3MzU1Mzk5fQ==";
+	String UPLOAD_TOKEN = "";
 	/**
 	 * 文件前缀分隔符
 	 */
